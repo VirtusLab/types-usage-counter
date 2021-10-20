4b2bc2d6bf9e0176de6ffaed38bc372c1699d096
Krzysztof Romanowski <romanowski.kr@gmail.com>
2021-10-20 02:44:22 +0200

    Clean up example code

--- spark.scala
+++ spark.scala
@@ -5,91 +5,82 @@
 // using repository https://wip-repos.s3.eu-central-1.amazonaws.com/.release
 // using io.github.vincenzobaz::spark-scala3:0.1.3-new-spark
 
-import scala3encoders.given 
+import scala3encoders.given
 
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Dataset
-import org.apache.log4j.{Level, LogManager, PropertyConfigurator}  
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._
+import org.apache.log4j.{Level, LogManager, PropertyConfigurator}
 import java.util.Base64
 
- @transient lazy val log = org.apache.log4j.LogManager.getLogger("TASTyJob")
+@transient lazy val log = org.apache.log4j.LogManager.getLogger("TASTyJob")
 
-case class SerializedTastyFile(lib: Library, path: String, contentBase64: String):
-  def toTastyFile = TastyFile(lib, path, Base64.getDecoder().decode(contentBase64))
-  //Base64.getEncoder().encodeToString(bytes)
+case class SerializedTastyFile(
+    lib: Library,
+    path: String,
+    contentBase64: String
+):
+  def toTastyFile =
+    TastyFile(lib, path, Base64.getDecoder().decode(contentBase64))
 
 object SerializedTastyFile:
   def from(tastyFile: TastyFile) =
-    SerializedTastyFile(
-      tastyFile.lib, 
-      tastyFile.path, 
-      Base64.getEncoder().encodeToString(tastyFile.content)
-    )
+    val encoded = Base64.getEncoder().encodeToString(tastyFile.content)
+    SerializedTastyFile(tastyFile.lib, tastyFile.path, encoded)
 
-case class SerializedTreeInfo(lib: Library, sourceFile: String, 
-  method: String, treeKind: String, index: Int, depth: Int, topLevelType: String)
+case class SerializedTreeInfo(
+    lib: Library,
+    sourceFile: String,
+    method: String,
+    treeKind: String,
+    index: Int,
+    depth: Int,
+    topLevelType: String
+)
 
 val NoType = "<no-type>"
 
-def processLibraries(libs: Dataset[Library]): Dataset[SerializedTreeInfo] = 
+object SerializedTreeInfo:
+  def from(info: TreeInfo) =
+    SerializedTreeInfo(
+      info.lib,
+      info.sourceFile,
+      info.method,
+      info.treeKind,
+      info.index,
+      info.depth,
+      info.topLevelType.getOrElse(NoType)
+    )
+
+def processLibraries(libs: Dataset[Library]): Dataset[SerializedTreeInfo] =
   libs
-    .flatMap { lib => 
-      val tastyFiles = loadTastyFiles(lib)
+    .flatMap { lib =>
+      val tastyFiles: Either[String, Seq[TastyFile]] = loadTastyFiles(lib)
       tastyFiles.left.foreach(log.warn)
-      tastyFiles.fold(_ => Nil, identity).map(SerializedTastyFile.from) 
+      tastyFiles.toSeq.flatten.map(SerializedTastyFile.from)
     }
-    .flatMap { serialized => 
-      processTastyFile(serialized.toTastyFile) match
-        case util.Left(msg) =>
-          log.warn(s"Error occured during TASTy processing: $msg")
-          Nil
-        case util.Right(infos) =>
-          infos.map( info =>
-            SerializedTreeInfo(info.lib, info.sourceFile, info.method, info.treeKind, info.index, info.depth, info.topLevelType.getOrElse(NoType))
-          )
+    .flatMap { serialized =>
+      val treeInfos: Either[String, Seq[TreeInfo]] =
+        processTastyFile(serialized.toTastyFile)
+      treeInfos.left.foreach(log.warn)
+      treeInfos.toSeq.flatten.map(SerializedTreeInfo.from)
     }
 
-@main def spark(args: String*) = 
-  val (csvPath , limit) = args match 
-    case Seq(s) => (s, None)
-    case Seq(s, i) => (s, Some(i.toInt))
-    case args => throw Exception("Usage: '<csv-file> [<limit>]'")
+@main def spark(args: String*) =
+  val csvPath = args.headOption.getOrElse("libs.csv")
 
-  lazy val spark: SparkSession = {
-    SparkSession
-      .builder()
-      .master("local")
-      .appName("spark test example")
-      .getOrCreate()
-  }
-
-  val sqlContext = new org.apache.spark.sql.SQLContext(spark.sparkContext)
-  import sqlContext.implicits._
-  import org.apache.spark.sql.functions._
+  val spark = SparkSession.builder().master("local").getOrCreate()
+  import spark.implicits._
 
   val libs = spark.read.option("header", true).csv(csvPath).as[Library]
+  val treeInfos = processLibraries(libs)
 
-  val limited = limit.fold(libs)(libs.limit(_))
-
-  val treeInfos = processLibraries(limited.toDF.as[Library])
-
-  val libSizes = treeInfos
-    .map {
-      case SerializedTreeInfo(lib, _, _, _, _, _, _) => s"${lib.org}:${lib.name.stripSuffix("_3")}"
-    }
-    .withColumnRenamed("value", "library")
-    .groupBy("library")
-    .count()
-    .sort(col("count").desc)
-
-  libSizes.show(100, false)
-
-  val counts = treeInfos
+  treeInfos
     .filter(col("topLevelType").=!=(NoType))
     .groupBy("topLevelType")
     .count()
     .sort(col("count").desc)
-
-  counts.show(100, false)
+    .show(10, false)
 
   spark.stop()
--- tasty.scala
+++ tasty.scala
@@ -3,6 +3,9 @@
 
 import java.io.File
 
+import util.Try
+import util.Using
+
 import tastyquery.reader.TastyUnpickler
 import tastyquery.Contexts
 import tastyquery.api.ProjectReader
@@ -15,104 +18,151 @@ import java.io.ByteArrayInputStream
 import java.net.URL
 import java.io.ByteArrayOutputStream
 import javax.naming.spi.DirStateFactory.Result
+import java.io.InputStream
 
-case class Library(org: String, name: String, version: String)
+case class Library(org: String, name: String, version: String):
+  def moduleString = s"$org:${name.stripSuffix("_3")}"
+
 case class TastyFile(lib: Library, path: String, content: Array[Byte])
-case class TreeInfo(lib: Library, sourceFile: String, 
-  method: String, treeKind: String, index: Int, depth: Int, topLevelType: Option[String])
+case class TreeInfo(
+    lib: Library,
+    sourceFile: String,
+    method: String,
+    treeKind: String,
+    index: Int,
+    depth: Int,
+    topLevelType: Option[String]
+)
 
-def loadTastyFiles(lib: Library): Either[String, Seq[TastyFile]] = 
+def loadLibrary(lib: Library): Either[String, InputStream] =
   val orgPath = lib.org.split('.').mkString("/")
-  val address = 
-    s"https://repo1.maven.org/maven2/$orgPath/${lib.name}/${lib.version}/${lib.name}-${lib.version}.jar"
-  try
-    val urlIs = URL(address).openStream();
-    try 
+  val repo = "https://repo1.maven.org/maven2"
+  val address =
+    s"$repo/$orgPath/${lib.name}/${lib.version}/${lib.name}-${lib.version}.jar"
+  try Right(URL(address).openStream())
+  catch
+    case e: java.io.FileNotFoundException =>
+      Left(s"Artifact not present: $address")
+
+def loadTastyFiles(lib: Library): Either[String, Seq[TastyFile]] =
+  loadLibrary(lib).flatMap { urlIs =>
+    try
       val zipIs = ZipInputStream(urlIs)
-      val tastyEntries = LazyList.continually(zipIs.getNextEntry).takeWhile(_ != null).filter(_.getName.endsWith(".tasty"))
+      val tastyEntries = LazyList
+        .continually(zipIs.getNextEntry)
+        .takeWhile(_ != null)
+        .filter(_.getName.endsWith(".tasty"))
+
       val tastyFiles = tastyEntries.map { entry =>
         val out = ByteArrayOutputStream()
         val buffer = new Array[Byte](4096)
-        LazyList.continually(zipIs.read(buffer)).takeWhile(_ != -1).foreach(out.write(buffer, 0, _))
+        LazyList
+          .continually(zipIs.read(buffer))
+          .takeWhile(_ != -1)
+          .foreach(out.write(buffer, 0, _))
         TastyFile(lib, entry.getName, out.toByteArray)
       }
       Right(tastyFiles.toList)
     finally urlIs.close()
-  catch
-    case e: java.io.FileNotFoundException => Left(s"Artifact not present: $address")
-
+  }
 
 def printType(tpe: Type): String = tpe match
-  case PackageRef(pck) => pck.toString
+  case PackageRef(pck)     => pck.toString
   case TypeRef(base, name) => printType(base) + "." + name.toString
-  case ThisType(base) => printType(base) + ".this"
-  case TermRef(base,name) => printType(base) + "." + name.toString
-  case AppliedType(base, args) => printType(base) + args.map(_ => "_").mkString("[", ", ", "]")
-  case NoPrefix => "<root>"
+  case ThisType(base)      => printType(base) + ".this"
+  case TermRef(base, name) => printType(base) + "." + name.toString
+  case AppliedType(base, args) =>
+    printType(base) + args.map(_ => "_").mkString("[", ", ", "]")
+  case NoPrefix              => "<root>"
   case AnnotatedType(tpe, _) => printType(tpe)
-  case AndType(tpe1, tpe2) => printType(tpe1) + " & " + printType(tpe2)
-  case OrType(tpe1, tpe2) => printType(tpe1) + " | " + printType(tpe2)
-  case _ => tpe.toString
+  case AndType(tpe1, tpe2)   => printType(tpe1) + " & " + printType(tpe2)
+  case OrType(tpe1, tpe2)    => printType(tpe1) + " | " + printType(tpe2)
+  case _                     => tpe.toString
 
 def isInteresting(tpe: Type): Boolean = tpe match
   case _: PackageRef => false
-  case _ => true
+  case _             => true
 
 case class Result(index: Int, infos: Seq[TreeInfo]):
-      def ++:(infos: Seq[TreeInfo]) = copy(infos = infos ++ this.infos)
+  def ++:(infos: Seq[TreeInfo]) = copy(infos = infos ++ this.infos)
 
-
-def walkTasty(tastyFile: TastyFile, t: Tree, index: Int, depth: Int, method: String, topLevel: Boolean): Result =
-  def processChildren(newMethod: String = method, index: Int = index, newTopLevel: Boolean = topLevel) =
-    t.subtrees.foldLeft(Result(index, Nil)){ (prev, tree) =>
-        prev.infos ++: walkTasty(tastyFile, tree, prev.index, depth + 1, newMethod, topLevel)
+def walkTasty(
+    tastyFile: TastyFile,
+    t: Tree,
+    index: Int,
+    depth: Int,
+    method: String,
+    topLevel: Boolean
+): Result =
+  def processChildren(
+      newMethod: String = method,
+      index: Int = index,
+      newTopLevel: Boolean = topLevel
+  ) =
+    t.subtrees.foldLeft(Result(index, Nil)) { (prev, tree) =>
+      prev.infos ++: walkTasty(
+        tastyFile,
+        tree,
+        prev.index,
+        depth + 1,
+        newMethod,
+        topLevel
+      )
     }
 
   def ignore = Result(index, Nil)
 
-  def newMethod(name: Name) = 
+  def newMethod(name: Name) =
     if method.isEmpty then name.toString else s"$method.$name"
 
-  def currentInfo =  TreeInfo(
-    tastyFile.lib, 
-    tastyFile.path, 
-    method, 
+  def currentInfo = TreeInfo(
+    tastyFile.lib,
+    tastyFile.path,
+    method,
     t.getClass.getSimpleName,
-    depth, 
-    index, 
+    depth,
+    index,
     t.tpeOpt.filter(isInteresting).map(printType)
   )
 
   t match
-    case PackageDef(pid, stats)                   => processChildren(newMethod(pid.name))
+    case PackageDef(pid, stats) => processChildren(newMethod(pid.name))
     case ImportSelector(imported, renamed, bound) => ignore
     case Import(expr, selectors)                  => ignore
-    case Class(name, rhs, symbol)                 => processChildren(newMethod(name))
-    case _: Template | _: Block                   => processChildren()
-    case ValDef(name, tpt, rhs, symbol)           => processChildren(newMethod(name), newTopLevel = false)
-    case DefDef(name, params, tpt, rhs, symbol)   => processChildren(newMethod(name), newTopLevel = false)
-    case _                                        => Seq(currentInfo) ++: processChildren(index = index + 1)
+    case Class(name, rhs, symbol) => processChildren(newMethod(name))
+    case _: Template | _: Block   => processChildren()
+    case ValDef(name, tpt, rhs, symbol) =>
+      processChildren(newMethod(name), newTopLevel = false)
+    case DefDef(name, params, tpt, rhs, symbol) =>
+      processChildren(newMethod(name), newTopLevel = false)
+    case _ => Seq(currentInfo) ++: processChildren(index = index + 1)
 
-def processTastyFile(tastyFile: TastyFile): Either[String, Seq[TreeInfo]] = 
-  util.Try {
-    val pickle = new TastyUnpickler(tastyFile.content).unpickle(new TastyUnpickler.TreeSectionUnpickler())
+def processTastyFile(tastyFile: TastyFile): Either[String, Seq[TreeInfo]] =
+  Try {
+    val pickle = new TastyUnpickler(tastyFile.content).unpickle(
+      new TastyUnpickler.TreeSectionUnpickler()
+    )
     val ast = pickle.get.unpickle(using Contexts.empty(tastyFile.path))
-    ast.flatMap(walkTasty(tastyFile, _ , 0, 0, "", topLevel = true).infos)
-  }.toEither.left.map(e => s"Failure to process ${tastyFile.path} from ${tastyFile.lib}: ${e.getMessage}")
+    ast.flatMap(walkTasty(tastyFile, _, 0, 0, "", topLevel = true).infos)
+  }.toEither.left
+    .map(e =>
+      s"Failure to process ${tastyFile.path} from ${tastyFile.lib}: ${e.getMessage}"
+    )
 
 @main def typesFrom(args: String*) = args match
   case Seq(org, name, version) =>
     val lib = Library(org, name, version)
-    val (failed, tastyFiles) = 
+    val (failed, tastyFiles) =
       val tastyFiles = loadTastyFiles(lib)
       tastyFiles.left.foreach(println)
-      tastyFiles.fold(_ => Nil, identity).map(processTastyFile)
+      tastyFiles
+        .fold(_ => Nil, identity)
+        .map(processTastyFile)
         .partitionMap(identity)
     failed.foreach(println)
     tastyFiles.flatten.take(10).foreach(println)
-    val mostPopularTypes =  
-      tastyFiles
-        .flatten
+    val mostPopularTypes =
+      tastyFiles.flatten
         .groupBy(_.topLevelType)
         .toSeq
         .collect { case (Some(tpe), instances) => (tpe, instances.size) }
@@ -120,21 +170,26 @@ def processTastyFile(tastyFile: TastyFile): Either[String, Seq[TreeInfo]] =
         .take(10)
     import scala.util.chaining._
     val librarySizes =
-      tastyFiles
-        .flatten
+      tastyFiles.flatten
         .groupBy(_.lib)
         .toSeq
-        .map {
-          case (lib, infos) => (s"${lib.org}:${lib.name}:${lib.version}", infos.size)
+        .map { case (lib, infos) =>
+          (s"${lib.org}:${lib.name}:${lib.version}", infos.size)
         }
         .sortBy(_(1))
         .reverse
 
     println("\nAmount of treeinfos per library:")
-    librarySizes.foreach {
-      case (lib, size) => println(s"$lib: $size")
+    librarySizes.foreach { case (lib, size) =>
+      println(s"$lib: $size")
     }
     println("\nMost popular types:")
-    println(mostPopularTypes.map{ case (name, count) => s"$name : $count"}.mkString("\n"))
+    println(
+      mostPopularTypes
+        .map { case (name, count) => s"$name : $count" }
+        .mkString("\n")
+    )
   case other =>
-        println(s"Expected <organization> <name> <version> but got ${args.mkString(" ")}") 
+    println(
+      s"Expected <organization> <name> <version> but got ${args.mkString(" ")}"
+    )
